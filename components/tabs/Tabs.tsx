f61f91a319d073abbe398d5c4fa4197902738375
Andy Chen <chenyangpoppin@hotmail.com>
2023-06-13 23:34:29 +0800

    feat: tab logic phase 1

--- components/tabs/Tabs.tsx
+++ components/tabs/Tabs.tsx
@@ -1,22 +1,55 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import styles from "./Tabs.module.scss";
+
+export interface TabHeader {
+  company: string;
+  logo?: string;
+}
+
+export interface TabBody {
+  role: string;
+  start: string;
+  end: string;
+  responsibilities: string[];
+}
 
 export interface TabData {
-  header: string[];
-  body: string[];
+  header: TabHeader;
+  body: TabBody;
 }
 
 const Tabs = ({
   data,
   onTabClick,
 }: {
-  data: TabData;
+  data: TabData[];
   onTabClick: () => void;
 }) => {
   useEffect(() => {
     console.log(data);
   }, []);
 
-  return <div onClick={onTabClick}>Tabs</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        {data.map((tab: TabData, index: number) => (
+          <div key={index} className={styles.header_item}>
+            {tab?.header.company}
+          </div>
+        ))}
+      </div>
+      <div className={styles.body}>
+        {data.map((tab: TabData, index: number) => (
+          <div key={index} className={styles.body_item}>
+            <div>{tab?.body.role}</div>
+            {tab?.body.responsibilities.map((task: string, idx: number) => (
+              <div key={idx}>{task}</div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tabs;
