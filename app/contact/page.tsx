df738b74f8470675b84f271ed1078287440dceb8
Andy Chen <chenyangpoppin@hotmail.com>
2023-06-18 16:24:15 +0800

    style: adapt style for /contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,30 +5,35 @@ import Link from "next/link";
 const Contact = () => {
   return (
     <main className={styles.main}>
-      <h1>WELCOME TO BUILD A CONNECTION WITH ME</h1>
-      <div className={styles.contact}>
-        <h1>Contact</h1>
-        <p>
-          <strong>Location:</strong> Ontario, Canada
-        </p>
-        <p>
-          <strong>Email:</strong> tjcchen.engineer@gmail.com
-        </p>
-      </div>
-      <div className={styles.media}>
-        <h1>Social Media</h1>
-        <p>
-          <strong>GitHub:</strong>{" "}
-          <Link href="https://github.com/tjcchen" target="_blank">
-            https://github.com/tjcchen
-          </Link>
-        </p>
-        <p>
-          <strong>LinkedIn:</strong>{" "}
-          <Link href="https://linkedin.com/in/andy-chen-engineer/" target="_blank">
-            https://linkedin.com/in/andy-chen-engineer/
-          </Link>
-        </p>
+      <h1 className={styles.caption}>WELCOME TO BUILD A CONNECTION WITH ME</h1>
+      <div className={styles.box}>
+        <div className={styles.contact}>
+          <h2>Contact</h2>
+          <p>
+            <strong>Location:</strong> Ontario, Canada
+          </p>
+          <p>
+            <strong>Email:</strong> tjcchen.engineer@gmail.com
+          </p>
+        </div>
+        <div className={styles.media}>
+          <h2>Social Media</h2>
+          <p>
+            <strong>GitHub:</strong>{" "}
+            <Link href="https://github.com/tjcchen" target="_blank">
+              https://github.com/tjcchen
+            </Link>
+          </p>
+          <p>
+            <strong>LinkedIn:</strong>{" "}
+            <Link
+              href="https://linkedin.com/in/andy-chen-engineer/"
+              target="_blank"
+            >
+              https://linkedin.com/in/andy-chen-engineer/
+            </Link>
+          </p>
+        </div>
       </div>
     </main>
   );
