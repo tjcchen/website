9dcce1d21ecbb0081bf1e1fd3d9432a4e9949aa6
Andy Chen <chenyangpoppin@hotmail.com>
2023-08-27 22:47:31 +0800

    style: update header color

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>&copy; 2023 TJCCHEN.COM All rights reserved.</div>
-      <div>This website is powered by Nginx, Docker, and Amazon AWS.</div>
+      <div>This website is powered by Nginx, Docker and Amazon AWS.</div>
     </footer>
   );
 };
