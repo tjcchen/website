81121c9e9823816d797fc092e7acc5767c652590
Andy Chen <chenyangpoppin@hotmail.com>
2023-10-29 23:01:05 +0800

    feat: add smooth BackToTop component logic

--- components/back-to-top/BackTop.tsx
+++ components/back-to-top/BackTop.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdArrowUpward } from "react-icons/md";
 import styles from "./BackTop.module.scss";
 
+const THRESHOLD_VALUE = 180;
+
 const BackTop = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  // backToTop button click
   const handleClick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const handleScroll = () => {
+    const currentScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    currentScroll > THRESHOLD_VALUE ? setIsActive(true) : setIsActive(false);
+  };
+
+  // onmount
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    // remove event listener when useEffect hook is removed
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.backTop} onClick={handleClick}>
+    <div
+      className={`${styles.backTop} ${isActive ? styles.active : ""}`}
+      onClick={handleClick}
+    >
       <MdArrowUpward color="black" fontSize="24px" />
     </div>
   );
