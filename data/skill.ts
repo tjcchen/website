a702c5488a2533bdc04600655b94f95db92718f2
Andy Chen <chenyangpoppin@hotmail.com>
2024-05-30 14:45:45 +0800

    Update website content

--- data/skill.ts
+++ data/skill.ts
@@ -11,7 +11,7 @@ const data = {
       "Java, C#, Python, Go, JavaScript, NodeJS, TypeScript, HTML, CSS, SQL",
     framework:
       "Go/Fiber, Python/Flask, NextJS, ReactJS, React Native, VueJS, RequireJS, Express, Spring Boot, Servlet, Velocity, Microservice, RPC, ASP.NET, MVC, MVVM, WPF, SOA",
-    databases: "PostgreSQL, MySQL, SQLServer, MongoDB, Redis",
+    databases: "PostgreSQL, MySQL, SQL Server, MongoDB, Redis",
     tools:
       "Linux, GitHub, GitLab, Git, Docker, Nginx, Amazon AWS, Alibaba Cloud, Webpack, Gulp, Gradle, Markdown",
   },
